1db5fd83b3f56710258ba38616099533d6d9e107
Tintin <yuijuhn.ting@titansoft.com.sg>
2023-04-05 06:59:56 +0800

    refactor: extract methods

--- test/CurrencyInput.test.ts
+++ test/CurrencyInput.test.ts
@@ -16,39 +16,44 @@ describe("PaymentMethodArea", () => {
         await wrapper.setProps({ modelValue: 1000, currency: 'THB' });
 
         expect(input.element.value).toBe('1,000');
-        expect(wrapper.emitted('update:modelValue')!.at(-1)).toEqual([1000]);
+        vModelAmountShouldBe(1000);
 
-        input.element.value = '8888';
-        await input.trigger('input');
+        await triggerInput('8888');
 
         expect(input.element.value).toBe('8,888');
-        expect(wrapper.emitted('update:modelValue')!.at(-1)).toEqual([8888]);
+        vModelAmountShouldBe(8888);
 
-        input.element.value = '9,999';
-        await input.trigger('input');
+        await triggerInput('9,999');
 
         expect(input.element.value).toBe('9,999');
-        expect(wrapper.emitted('update:modelValue')!.at(-1)).toEqual([9999]);
+        vModelAmountShouldBe(9999);
 
-        input.element.value = '9.999';
-        await input.trigger('input');
+        await triggerInput('9.999');
 
         expect(input.element.value).toBe('10');
-        expect(wrapper.emitted('update:modelValue')!.at(-1)).toEqual([10]);
+        vModelAmountShouldBe(10);
     });
 
     test('BRL, VND should see different decimal sign', async () => {
         await wrapper.setProps({ modelValue: 1000, currency: 'VND' });
 
         expect(input.element.value).toBe('1.000');
-        expect(wrapper.emitted('update:modelValue')!.at(-1)).toEqual([1000]);
+        vModelAmountShouldBe(1000);
     });
 
     test('USDT should see default decimal sign', async () => {
         await wrapper.setProps({ modelValue: 1000, currency: 'USDT' });
 
-        expect(wrapper.find('input').element.value).toBe('1,000');
-        expect(wrapper.emitted('update:modelValue')!.at(-1)).toEqual([1000]);
+        expect(input.element.value).toBe('1,000');
+        vModelAmountShouldBe(1000);
     });
 
+    function vModelAmountShouldBe(modelValue: Number) {
+        expect(wrapper.emitted('update:modelValue')!.at(-1)).toEqual([modelValue]);
+    }
+
+    async function triggerInput(value: string) {
+        input.element.value = value;
+        await input.trigger('input');
+    }
 });
