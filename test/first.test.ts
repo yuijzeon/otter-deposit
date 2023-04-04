e62ea8c7617ea5d946da138a3c30cd0a2f057473
Tintin <yuijuhn.ting@titansoft.com.sg>
2023-04-04 16:06:12 +0800

    first test

--- test/first.test.ts
+++ test/first.test.ts
@@ -1,4 +1,37 @@
+// @vitest-environment jsdom
 import { test, expect } from 'vitest'
-test('one should be one', async () => {
-    expect(1).toBe(1);
-})
+import { mount } from "@vue/test-utils";
+import CurrencyInput from "../src/components/CurrencyInput.vue";
+
+test('BRL, VND should see diff decimal sign', async () => {
+    const propsData: { modelValue: number, currency: String } = {
+        modelValue: 1000,
+        currency: 'VND'
+    };
+
+    const wrapper = mount(CurrencyInput, { propsData });
+    const input = wrapper.find('input');
+
+    expect(input.element.value).toBe('1.000');
+
+    input.element.value = '9999';
+    await input.trigger('input');
+
+    expect(input.element.value).toBe('9.999');
+
+    input.element.value = '9,999';
+    await input.trigger('input');
+
+    expect(input.element.value).toBe('10');
+});
+
+test('USDT should see default decimal sign', () => {
+    const propsData: { modelValue: number, currency: String } = {
+        modelValue: 1000,
+        currency: 'USDT'
+    };
+
+    const wrapper = mount(CurrencyInput, { propsData });
+
+    expect(wrapper.find('input').element.value).toBe('1,000');
+});
