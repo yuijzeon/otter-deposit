4769f191fdadb9a598da33c04d14d32de3a60682
Tintin <yuijuhn.ting@titansoft.com.sg>
2023-04-04 19:32:53 +0800

    refactor: rename test file

--- test/CurrencyInput.test.ts
+++ test/CurrencyInput.test.ts
@@ -3,6 +3,33 @@ import { test, expect } from 'vitest'
 import { mount } from "@vue/test-utils";
 import CurrencyInput from "../src/components/CurrencyInput.vue";
 
+test('format number', async () => {
+    const propsData: { modelValue: Number, currency: String } = {
+        modelValue: 1000,
+        currency: 'THB'
+    };
+
+    const wrapper = mount(CurrencyInput, { propsData });
+    const input = wrapper.find('input');
+
+    expect(input.element.value).toBe('1,000');
+
+    input.element.value = '8888';
+    await input.trigger('input');
+
+    expect(input.element.value).toBe('8,888');
+
+    input.element.value = '9,999';
+    await input.trigger('input');
+
+    expect(input.element.value).toBe('9,999');
+
+    input.element.value = '9.999';
+    await input.trigger('input');
+
+    expect(input.element.value).toBe('10');
+});
+
 test('BRL, VND should see different decimal sign', async () => {
     const propsData: { modelValue: Number, currency: String } = {
         modelValue: 1000,
@@ -13,16 +40,6 @@ test('BRL, VND should see different decimal sign', async () => {
     const input = wrapper.find('input');
 
     expect(input.element.value).toBe('1.000');
-
-    input.element.value = '9999';
-    await input.trigger('input');
-
-    expect(input.element.value).toBe('9.999');
-
-    input.element.value = '9,999';
-    await input.trigger('input');
-
-    expect(input.element.value).toBe('10');
 });
 
 test('USDT should see default decimal sign', () => {
